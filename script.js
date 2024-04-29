936ce21df09193937e7bb7ba2fc77843052ac6b4
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-29 19:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    // Access the table
+    var table = document.getElementById("sampleTable");
+
+    // Create a new row
+    var row = table.insertRow(0);
+
+    // Create new cells in the row
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    
+    // Add content to the cells
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
